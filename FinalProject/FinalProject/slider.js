7e236ed7bc946be99af04099e233274d897b1b18
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2024-03-16 14:34:47 +0400

    fix carousel scroll

--- FinalProject/FinalProject/slider.js
+++ FinalProject/FinalProject/slider.js
@@ -1,30 +1,12 @@
-const gap = 16;
+const carouselContainer = document.getElementById("carousel");
+const carouselContent = document.getElementById("carouselContent");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
 
-const carousel = document.getElementById("carousel"),
-  content = document.getElementById("carouselContent"),
-  next = document.getElementById("next"),
-  prev = document.getElementById("prev");
-
-next.addEventListener("click", e => {
-  console.log("rightClick");
-  carousel.scrollBy(width + gap, 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "none";
-  }
-});
-prev.addEventListener("click", e => {
-  console.log("leftClick");
-  carousel.scrollBy(-(width + gap), 0);
-  if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = "none";
-  }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
-  }
+nextBtn.addEventListener("click", () => {
+    carouselContainer.scrollLeft += carouselContainer.offsetWidth;
 });
 
-let width = carousel.offsetWidth;
-window.addEventListener("resize", e => (width = carousel.offsetWidth));
+prevBtn.addEventListener("click", () => {
+    carouselContainer.scrollLeft -= carouselContainer.offsetWidth;
+});
