0d17d59c56bb7c5b8700a5e9d190f903bcb16ac0
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2024-03-16 14:52:46 +0400

    check for user login

--- FinalProject/FinalProject/auth.js
+++ FinalProject/FinalProject/auth.js
@@ -1,3 +1,9 @@
+document.body.addEventListener("click", function() {
+  if (!sessionStorage.getItem('isLoggedIn') && window.location.pathname !== "/authorization.html") {
+      window.location.href = "register.html";
+  }
+});
+
 function authorizeUser() {
     const savedUsers = JSON.parse(localStorage.getItem('users')) || [];
     const enteredEmail = document.getElementById('username').value; // Assuming 'username' is the email
@@ -7,10 +13,12 @@ function authorizeUser() {
   
     if (user) {
       alert('User authorized successfully!');
+      sessionStorage.setItem('isLoggedIn', 'true');
       window.location.href = 'index.html'; // Redirect on success
       return true;
     } else {
       alert('Invalid email or password. Please try again.');
+      sessionStorage.setItem('isLoggedIn', 'false');
       return false;
     }
   }
